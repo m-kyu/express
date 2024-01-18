3a2a1719113dcba0b9c5b11cfe7b7ce4b202cad5
m-kyu <yicha7@gmail.com>
2024-01-18 18:20:44 +0900

    2

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const fs = require('fs');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+let data = JSON.parse(fs.readFileSync('./data.json'));
+ 
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/test', function (req, res) {
+    res.send(data.test);
+})
+
+app.get('/test/:id', function (req, res) {
+    const {id} = req.params;
+    const findData = data.test.find(obj=>obj.id==id);
+    res.send(findData);
+})
+
+app.post('/test', function (req, res) {
+    data.test.push(req.body);
+    const body = JSON.stringify(data);    
+    const dataInsert = fs.writeFileSync('./data.json',body);
+    res.send(data.test);
+})
+
+app.delete('/test/:id', function (req, res) {
+    const {id} = req.params;
+    data.test = data.test.filter(obj=>obj.id != id)
+    const body = JSON.stringify(data);    
+    fs.writeFileSync('./data.json',body);
+    res.send(data.test);
+})
+
+
+app.put('/test/', function (req, res) {
+    const updateBody = req.body;
+
+    data.test = data.test.map(obj=> {
+        if(obj.id == updateBody.id){ 
+            obj = updateBody;
+        }
+        return obj;
+    })
+    const body = JSON.stringify(data);    
+    fs.writeFileSync('./data.json',body);
+    res.send(data.test);
+})
+
+app.listen(3030)
+
