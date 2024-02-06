951907ce0d8b98bdd7f4d6a4843729b258be0f98
m-kyu <yicha7@gmail.com>
2024-02-06 14:37:33 +0900

    sdfsdf

--- router/camera.js
+++ router/camera.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const cameraRouter = require('express').Router();
 const path = require('path');
+
+
 const multer = require('multer');
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -8,28 +10,42 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    console.log(file)
     cb(null, file.originalname)
   }
 })
 
+const uploadDir = path.join(path.join(__dirname,'..') + '/uploads/');
 const upload = multer({ storage: storage })
 
-cameraRouter.get('/', async function (req, res) {
-  res.send('camera ready...');
-})
-
 cameraRouter.get('/photo/:img', async function (req, res) {
-    res.sendFile(path.join(__dirname,'..') + '/uploads/' + req.params.img)
+    res.sendFile(uploadDir + req.params.img)
 })
+
+
 
 cameraRouter.get('/', async function (req, res) {
   res.send('camera ready...');
 })
+
+
 
 
 cameraRouter.post('/save', upload.single('image'), function (req, res) {
-  res.send('성공')
+  const file = req.file;
+  const imageUrl = {fileUrl : file.filename};
+  res.send(imageUrl)
+})
+
+cameraRouter.delete('/photo/:filename',  function (req, res) {
+    const filename = req.params.filename;
+    const filePath = path.join(uploadDir, filename);
+
+    if (fs.existsSync(filePath)) {
+      fs.unlinkSync(filePath);
+      res.send('지우기성공!!');
+    } else {
+      res.status(404).send('File not found');
+    }
 })
 
 module.exports = cameraRouter;
