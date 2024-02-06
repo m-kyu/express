7f2c67ea34d3ffbded29ad8c06b7e4fd7f33b2b8
m-kyu <yicha7@gmail.com>
2024-02-06 09:46:19 +0900

    sdfsf

--- router/camera.js
+++ router/camera.js
@@ -1,8 +1,20 @@
 const fs = require('fs');
 const cameraRouter = require('express').Router();
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
-//upload.single('image')
+// const upload = multer({ dest: 'uploads/' });
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    console.log(file)
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({ storage: storage })
 
 cameraRouter.get('/', async function (req, res) {
     res.send('camera ready...');
